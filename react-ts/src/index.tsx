356cc625ba5d46d91060779f64980c0d15b36376
Andrew Burnett <andrew@andburn.info>
2018-01-30 22:57:17 +0000

    Clean up defaults & add enzyme

--- react-ts/src/index.tsx
+++ react-ts/src/index.tsx
@@ -3,12 +3,10 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import Hello from './components/Hello';
 
 ReactDOM.render(
   <div>
     <App />
-    <Hello name="TypeScript" enthusiasmLevel={5} />
   </div>,
   document.getElementById('root') as HTMLElement
 );
